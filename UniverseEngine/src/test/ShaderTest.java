eb4a873914190884804ce2a2a9b2e3f1020facad
Philipp Krueger <matheus.dev@googlemail.com>
2012-03-27 19:38:13 +0200

    Added missing class file for last commit

---
+++ UniverseEngine/src/test/ShaderTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.lwjgl.opengl.GL11;
+import org.universeengine.mains.UniverseEngineShaderTest;
+import org.universeengine.mains.shadertests.UniShaderTestUniform;
+import org.universeengine.opengl.shader.UniShader;
+import org.universeengine.opengl.shader.UniShaderProgram;
+
+public class ShaderTest implements UniShaderTestUniform {
+	
+	private ShaderTest() {
+	}
+
+	public void init(UniShaderProgram prog, UniShader vert, UniShader frag) {
+	}
+
+	public void uniformAction() {
+	}
+	
+	public static void main(String[] args) {
+		UniverseEngineShaderTest.start("res/bunny.obj", GL11.GL_TRIANGLES, new ShaderTest());
+	}
+
+}
